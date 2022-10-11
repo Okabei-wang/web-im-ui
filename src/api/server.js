a2938e73a1435018293cc3b7216a606d643bee9b
Okabei <1050138672@qq.com>
2022-10-11 10:50:56 +0800

    修改环境

--- src/api/server.js
+++ src/api/server.js
@@ -1,3 +1,8 @@
-const serverHost = 'http://127.0.0.1:3000';
+let serverHost = 'http://127.0.0.1:3000';
+
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'production') {
+  serverHost = 'http://101.43.169.241:3001'
+}
 
 export { serverHost }
