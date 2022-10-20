9a548941394a0c8b692cacfb76e6d7be533f2d63
Okabei <1050138672@qq.com>
2022-10-20 11:11:22 +0800

    请求改为https

--- src/api/server.js
+++ src/api/server.js
@@ -2,7 +2,7 @@ let serverHost = 'http://127.0.0.1:3000';
 
 console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
-  serverHost = 'http://101.43.169.241:3000'
+  serverHost = 'https://101.43.169.241:3000'
 }
 
 export { serverHost }
